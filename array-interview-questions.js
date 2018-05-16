9f63800d911efc5ccca5c91ecfc725efdc10e3b9
midimurphdesigns <midimurphdesigns@gmail.com>
2018-05-16 11:26:22 -0700

    filter number array

--- array-interview-questions.js
+++ array-interview-questions.js
@@ -3,13 +3,35 @@
 
 // Write a method that takes in a string and replaces all its empty spaces with a '%20'. 
 
-function spaceChanger(string) {
-  for (let i=0; i < string.length; i++) {
-    if (string[i] === ' ') {
-      string = string.slice(0, i) + '%20' + string.slice(i+1);
-    }
-  }
-  return string;
-}
+// function spaceChanger(string) {
+//   for (let i=0; i < string.length; i++) {
+//     if (string[i] === ' ') {
+//       string = string.slice(0, i) + '%20' + string.slice(i+1);
+//     }
+//   }
+//   return string;
+// }
 
-console.log(spaceChanger('hello im kevin'));
+// console.log(spaceChanger('hello im kevin'));
+
+
+
+// Write an algorithm to remove all numbers less than five from the array.
+
+// function filterNum(arr) {
+//   let newArr = []
+//   if (!arr.length) {
+//     return newArr
+//   }
+//   for (let i=0; i < arr.length; i++) {
+//     if (arr[i] > 5) {
+//       newArr.push(arr[i]);
+//     }
+//   }
+//   return newArr;
+// }
+
+// console.log(filterNum([7, 9, 3, 6, 2, 1, 0, 12, 15, 19]))
+
+
+
