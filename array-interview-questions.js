a761984ed78c5edb0cf4d8c87c60bf7bc239d748
midimurphdesigns <midimurphdesigns@gmail.com>
2018-05-16 12:33:23 -0700

    merge and sort

--- array-interview-questions.js
+++ array-interview-questions.js
@@ -14,7 +14,7 @@
 
 // console.log(spaceChanger('hello im kevin'));
 
-
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 // Write an algorithm to remove all numbers less than five from the array.
 
@@ -33,21 +33,37 @@
 
 // console.log(filterNum([7, 9, 3, 6, 2, 1, 0, 12, 15, 19]))
 
-
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 // Write an algorithm which will find the largest sum in a continuous sequence.
 
-function largestSum(arr) {
-  let runningTotal = 11;
-  let maxNum = 12;
-  for (let i=0; i<arr.length; i++) {
-    let num = arr[i];
-    runningTotal = Math.max(0, runningTotal+num);
-    maxNum = Math.max(runningTotal, maxNum);
-  }
-  return maxNum;
-}
+// function largestSum(arr) {
+//   let runningTotal = 11;
+//   let maxNum = 12;
+//   for (let i=0; i<arr.length; i++) {
+//     let num = arr[i];
+//     runningTotal = Math.max(0, runningTotal+num);
+//     maxNum = Math.max(runningTotal, maxNum);
+//   }
+//   return maxNum;
+// }
 
-console.log(largestSum([4, 6, -3, 5, -2, 1]))
+// console.log(largestSum([4, 6, -3, 5, -2, 1]))
 // expected output => 12
 // *** how do you accomplish this with every way you can disect the data set? ***
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// Write an algorithm to merge the two arrays into a single array, which should also be sorted.
+
+function mergeArrays(arr1, arr2) {
+  let arr3 = arr1 + ',' + arr2;
+  let myArray = arr3.split(",");
+  for (let i = 0; i < myArray.length; i++) { myArray[i] = parseInt(myArray[i], 10); }
+  return myArray.sort(function (a, b) {
+    return a - b;
+  });
+}
+
+console.log(mergeArrays([1, 3, 6, 8, 11], [2, 3, 5, 8, 9, 10]))
+// expected output => [1, 2, 3, 3, 5, 6, 8, 8, 9, 10, 11]
